05ec1bde8f9da71977543257f4c2c0bdbcf37ea2
Glitch (glitch-hello-website) <none>
2023-08-20 20:00:10 +0000

    👻😋 Checkpoint ./index.html:59895810/3585 ./script.js:59895810/3245 ./style.css:59895810/4798

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+    const back = document.querySelector('#back');
+    const next = document.querySelector('#next');
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+    const photos = ["https://images.unsplash.com/photo-1606757819934-d61a9f7279d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80", "https://images.unsplash.com/photo-1610441009633-b6ca9c6d4be2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80", "https://images.unsplash.com/photo-1592417817098-8fd3d9eb14a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80", "https://images.unsplash.com/photo-1510629954389-c1e0da47d414?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80", "https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"];
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
+    let i = 0;
 
+    next.addEventListener('click', ()=> {
+        i++;
+            if (i > photos.length -1){
+            i=0;
+        }
+    document.querySelector('#pictures').src = photos[i];
+    })
 
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
+    back.addEventListener('click', () => {
+        i--;
+            if (i <0){
+            i = photos.length - 1;
+    }
 
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
-});
+    document.querySelector('#pictures').src = photos[i]
+
+    })
